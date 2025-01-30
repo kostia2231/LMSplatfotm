af2888650e51458d5beb60f10bdfd9765b763bc9
konstantin <kostia505@gmail.com>
2025-01-30 16:42:10 +0100

    clerk: added; header/paths

---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function ConsumerLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <header className="flex h-12 shadow bg-background z-10">
+      <nav className="flex gap-4 container">
+        <Link
+          className="mr-auto text-lg hover:underline items-center px-2 flex"
+          href="/"
+        >
+          Course Product
+        </Link>
+      </nav>
+    </header>
+  );
+}
