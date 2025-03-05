74667df18bc7abdaadb6aca38b79ac0b201de86a
konstantin <kostia505@gmail.com>
2025-03-05 15:26:48 +0100

    schadcn added; sign in added

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,5 +1,7 @@
+import { ReactNode, Suspense } from "react";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { Button } from "@/components/ui/button";
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function ConsumerLayout({
   children,
@@ -14,14 +16,40 @@ export default function ConsumerLayout({
 
 function Navbar() {
   return (
-    <header className="flex h-12 shadow bg-background z-10">
-      <nav className="flex gap-4 container">
+    <header className="flex h-15 shadow bg-background z-10">
+      <nav className="flex gap-4 container px-2">
         <Link
-          className="mr-auto text-lg hover:underline items-center px-2 flex"
+          className="hover:bg-accent/10 pr-2 mr-auto text-lg items-center flex"
           href="/"
         >
           Course Product
         </Link>
+        <Suspense>
+          <SignedIn>
+            <Link
+              className="hover:bg-accent/10 mr-auto text-lg items-center px-2 flex"
+              href="/courses"
+            >
+              My Courses
+            </Link>
+            <Link
+              className="hover:bg-accent/10 mr-auto text-lg items-center px-2 flex"
+              href="/purchases"
+            >
+              Purchase History
+            </Link>
+            <div className="size-8 self-center">
+              <UserButton appearance={{ elements: { userButtonAvatarBox: { width: "100%", height: "100%" } } }} />
+            </div>
+          </SignedIn>
+        </Suspense>
+        <Suspense>
+          <SignedOut>
+            <Button className="self-center" asChild>
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+          </SignedOut>
+        </Suspense>
       </nav>
     </header>
   );
