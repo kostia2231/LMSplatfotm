f1b7b9bc625a09d528b4b0bc4cf138e7c1c5417f
konstantin <kostia505@gmail.com>
2025-03-05 16:50:16 +0100

    featutures/users/db/users added

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return <><div className="min-h-screen flex flex-col justify-center items-center">
+    {children}
+  </div></>
+}
---
+++ src/features/users/db/users.ts
@@ -0,0 +1,36 @@
+import { UserTable } from "@/drizzle/schema";
+import { db } from "@/drizzle/db";
+import { eq } from "drizzle-orm";
+
+export async function insertUser(data: typeof UserTable.$inferInsert) {
+  const [newUser] = await db.insert(UserTable).values(data).returning().onConflictDoUpdate({
+    target: [UserTable.clerkUserId],
+    set: data
+  })
+
+  if (newUser == null) throw new Error("failed to create new user")
+
+  return newUser
+}
+
+export async function updateUser({ clerkUserId }: { clerkUserId: string }, data: Partial<typeof UserTable.$inferInsert>) {
+  const [updatedUser] = await db.update(UserTable).set(data).where(eq(UserTable.clerkUserId, clerkUserId)).returning()
+
+  if (updatedUser == null) throw new Error("failed to create new user")
+
+  return updatedUser
+}
+
+export async function deleteUser({ clerkUserId }: { clerkUserId: string }) {
+  const [deletedUser] = await db.update(UserTable).set({
+    deletedAt: new Date(),
+    email: "deleted@deleted.com",
+    name: "deleted user",
+    clerkUserId: "deleted",
+    imageUrl: null
+  }).where(eq(UserTable.clerkUserId, clerkUserId)).returning()
+
+  if (deletedUser == null) throw new Error("failed to create new user")
+
+  return deletedUser
+}
